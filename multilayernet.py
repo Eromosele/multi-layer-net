284f2cafc9e1fc11d26c20be75b0ee340de3f09d
Eromosele Inegbedion <eromoseleinegbe@gmail.com>
2019-07-22 23:28:01 +0100

    Update multilayernet.py

--- multilayernet.py
+++ multilayernet.py
@@ -1,6 +1,7 @@
 """
 multilayernet.py
 ~~~~~~~~~~~~~~~~
+---example network---
       |o|-->|o|
 |o|--> |     |
       |o|-->|o|-->|o| (2 features, 2 hidden layers, 1 output node)
@@ -11,13 +12,11 @@ A simple python implementation of a multi layer neural net(2 hidden).
 This implementation of the neural net is able to accept an arbitiary
 number of features and examples.
 
--data structure
--_-_-_-_-_-_-_-_-_
+---data structure---
 the inputs are structured with the features being the row and examples shared in columns
 example: if there is are 3 pieces of data (1, 2), (3, 4), (5, 6)
-from the data provided there are 2 features and 3 examples the data would be
-structured as so:
-
+from the data provided there are 2 features and 3 examples
+the data will be structures as so:
 5 3 1 --> o
 | | |
 6 4 2 --> o
@@ -30,8 +29,7 @@ x = np.array([[1, 2]]) (2 examples with 1 feature)
 x = np.array([[1], [1]) (1 example with 2 features)
 x = np.array([[1, 1], [1, 1]) (2 examples with 2 features)
 x = np.array([[1, 1, 1], [1, 1, 1]) (3 examples with 2 features)
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 y = np.array([[1]]) (output for 1 example)
 y = np.array([[1, 2]]) (output for 2 examples)
 
@@ -49,14 +47,39 @@ def sigmoid(x, der=False):
 def getshapeof(x):
     print(np.shape(x))
 
+column1_ = []
+column2_ = []
+column3_ = []
+column4_ = []
+column5_ = []
 
-x = np.array([[0, 1, 1, 0], [1, 1, 0, 0]]).T # input examples
+# comment out this section for custom examples
+with open('test_data.txt', 'r') as reader:
+    lines = [line.rstrip('\n') for line in reader]
+    for line in lines:
+        column1, column2, column3, column4, column5 = line.split(',')
+        column1_.append(float(column1))
+        column2_.append(float(column2))
+        column3_.append(float(column3))
+        column4_.append(float(column4))
+        column5_.append(float(column5))
+
+    x = np.array([(column1_), (column2_), (column3_), (column4_)]).T
+    y = np.array([(column5_)])
+
+    print(y)
+
+reader.close()
+
+
+# custom examples
+# x = np.array([[0, 1, 1, 0], [1, 1, 0, 0]]).T # input examples
+# y = np.array([[1, 1, 1, 0]])
 
 weight1 = np.random.random((x.shape[1], 8)) # weight between input and hidden layer 1
 weight2 = np.random.random((8, 8)) # weight between hidden layer 1 and 2
 weight3 = np.random.random((8, 1)) # weight between hidden layer 2 and output
 
-y = np.array([[1, 1, 1, 0]])
 
 # iterations
 for i in range(3000):
@@ -71,9 +94,9 @@ for i in range(3000):
 
     scalar = derived_error * sigmoid(np.dot(derived_hidden_layer2, weight3), True) # this remains static throught the calculations
 
-    weight3 += np.dot(scalar.T,derived_hidden_layer2).T * 1
-    weight2 += np.dot(np.dot(scalar,weight3.T).T, sigmoid(np.dot(derived_hidden_layer, weight2),True) * sigmoid(np.dot(x, weight1),True)) * 1
-    weight1 += np.dot((np.dot(scalar,weight3.T) * np.dot(sigmoid(np.dot(derived_hidden_layer, weight2),True),weight2) * sigmoid(np.dot(x, weight1),True)).T,x).T * 1
+    weight3 += np.dot(scalar.T,derived_hidden_layer2).T * 0.01
+    weight2 += np.dot(np.dot(scalar,weight3.T).T, sigmoid(np.dot(derived_hidden_layer, weight2),True) * sigmoid(np.dot(x, weight1),True)) * 0.01
+    weight1 += np.dot((np.dot(scalar,weight3.T) * np.dot(sigmoid(np.dot(derived_hidden_layer, weight2),True),weight2) * sigmoid(np.dot(x, weight1),True)).T,x).T * 0.01
 
     if i % 300 == 0:
         print("Error (%s'th iteration): %s" % (i,error))
